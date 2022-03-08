606ee9715365a2b39c6b6f6834bb0c030f973fe0
Charlie McClung <charlie@bowtie.co>
2022-03-08 11:29:00 -0700

    Add -y flag to automate yum setup in user data script

--- config/rpm-user-data.sh
+++ config/rpm-user-data.sh
@@ -3,13 +3,13 @@
 # Sockz Server UserData (init script)
 
 # Update package manager
-yum update
+yum update -y
 
 # Setup for nodejs v14 install (via yum next step)
 curl -sL https://rpm.nodesource.com/setup_14.x | bash -
 
 # Install required packages
-yum install git gcc-c++ make nodejs
+yum install -y git gcc-c++ make nodejs
 
 # Upgrade NPM to latest version
 npm install --global npm@latest
